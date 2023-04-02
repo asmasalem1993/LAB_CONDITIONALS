17832296950dd21180c5e3bb02827dfc21e0bbff
asmasalem1993 <129065082+asmasalem1993@users.noreply.github.com>
2023-04-02 10:52:13 +0300

    solve lab conditionals.

---
+++ lab.py
@@ -0,0 +1,18 @@
+my_best_movie = "Cinderella"
+#rating of the movie out of 5 
+rate_of_movie_from5 : int = 3 
+popularity_score : float = 72.65
+
+if rate_of_movie_from5 >= 4 and popularity_score >80: 
+    print ("Highly recommended")
+elif rate_of_movie_from5>=3 and popularity_score >70:
+   print("I recommended it . It is good")
+elif rate_of_movie_from5>=2 and popularity_score >60:
+    print("You should check it out!")
+else:
+    rate_of_movie_from5>=2 and popularity_score >50
+    print("Don't watch it. It is a waste of time")
+
+
+
+   
